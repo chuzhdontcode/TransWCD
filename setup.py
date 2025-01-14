a12e829773941a1f00bb530d998ebf38b34a6da5
Chu Zhen Hao <zhen-hao.chu@vitrox.com>
2025-01-14 08:38:36 +0000

    build: modify the versions of dependencies

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# Read the requirements from requirements.txt
+def parse_requirements(filename):
+    with open(filename, "r") as file:
+        return [line.strip() for line in file if line.strip() and not line.startswith("#")]
+
+setup(
+    name="transwcd",
+    version="0.0.0",
+    description="Describe Your Cool Project",
+    author="",
+    author_email="",
+    # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
+    url="",
+    install_requires=parse_requirements("transwcd/requirements.txt"),
+    packages=find_packages(),
+)
